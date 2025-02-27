7f44d7531de6db628b84b417cdcff79b19c1cd87
abarnes05 <alexiarbarnes@gmail.com>
2025-02-27 10:33:49 -0500

    Added the private member and public function declarations.

--- BingoBall.h
+++ BingoBall.h
@@ -4,5 +4,17 @@
 
 #ifndef BINGOBALL_H
 #define BINGOBALL_H
+#include <string>
+using std::ostream;
+
+class BingoBall {
+private:
+    char letter;
+    int number;
+
+public:
+    BingoBall();
+    friend ostream& operator << (ostream& outs, const BingoBall& ball);
+};
 
 #endif //BINGOBALL_H
